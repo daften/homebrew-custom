ebaa5ad6ce1edca1ae61ece233db4379470783ff
Dieter Blomme <dieterblomme@gmail.com>
2020-11-12 14:01:17 +0100

    Added composer1

---
+++ Formula/composer1.rb
@@ -0,0 +1,59 @@
+class Composer1 < Formula
+  desc "Dependency Manager for PHP, pinned to latest 1.x"
+  homepage "https://getcomposer.org/"
+  url "https://getcomposer.org/download/1.10.17/composer.phar"
+  sha256 "6fa00eba5103ce6750f94f87af8356e12cc45d5bbb11a140533790cf60725f1c"
+
+  bottle :unneeded
+
+  def install
+    bin.install "composer.phar" => "composer1"
+  end
+
+  test do
+    (testpath/"composer.json").write <<~EOS
+      {
+        "name": "homebrew/test",
+        "authors": [
+          {
+            "name": "Homebrew"
+          }
+        ],
+        "require": {
+          "php": ">=5.3.4"
+          },
+        "autoload": {
+          "psr-0": {
+            "HelloWorld": "src/"
+          }
+        }
+      }
+    EOS
+
+    (testpath/"src/HelloWorld/greetings.php").write <<~EOS
+      <?php
+
+      namespace HelloWorld;
+
+      class Greetings {
+        public static function sayHelloWorld() {
+          return 'HelloHomebrew';
+        }
+      }
+    EOS
+
+    (testpath/"tests/test.php").write <<~EOS
+      <?php
+
+      // Autoload files using the Composer autoloader.
+      require_once __DIR__ . '/../vendor/autoload.php';
+
+      use HelloWorld\\Greetings;
+
+      echo Greetings::sayHelloWorld();
+    EOS
+
+    system "#{bin}/composer1", "install"
+    assert_match /^HelloHomebrew$/, shell_output("php tests/test.php")
+  end
+end
